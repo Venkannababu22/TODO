f9efe7175dbae726dcdb87f041952aad2fe91818
VENKANNABABU <yellamellivenkannababu@gmail.com>
2024-04-19 03:32:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState([])
+  const [isClicked, setIsClicked] = useState(false);
+  
+  let bool = true;
+
+  const addText = (e) => {
+    setText(e.target.value);
+  }
+
+  const addDataToTable = () => {
+
+    if(text === ""){
+      alert("Please enter the task description")
+      return;
+    }
+    setIsClicked(true)
+    setData([...data, text]);
+    setStatus([...status, "Pending"])
+    setText("");
+  };
+
+  const deleteRow = (index) => {
+    setData(data.filter((_, i) => i !== index));
+    setStatus(status.filter((_, i) => i !== index));
+    if (data.length === 1) {
+      setIsClicked(false);
+    }
+  };
+  
+
+  const handleFinish = (index) => {
+    const newStatus = [...status];
+    // newStatus[index] = newStatus[index] === "Pending" ? "Completed" : "Pending"; 
+    newStatus[index] = "Completed"; 
+    setStatus(newStatus);
+    bool = false;
+  };
+ 
+ return (
+    <div className='container'>
+      <h1 className='title'>TO DO App</h1>
+      <div className='textSave'>
+        <input className='textArea' type='text' placeholder='Enter the task here' value={text} onChange={addText}/>
+        <button className='saveButton' onClick={addDataToTable}>Add</button>
+      </div>
+      <table>
+        <thead>
+          { isClicked &&
+          <tr className='tableHeadRow'>
+              <th id='one'>Serial no.</th>
+              <th id='two'>Todo Item</th>
+              <th id='three'>Status</th>
+              <th id='four'>Actions</th>
+          </tr>
+          }
+        </thead>
+        <tbody>
+        {data &&
+            data.map((item, i) => (
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{item}</td>
+                <td>{status[i]}</td>
+                <td className='hover'>
+                  <button className='btn' onClick={() => deleteRow(i)}>Delete</button>
+                  {bool &&  
+                    <button className='btn1' onClick={() =>handleFinish(i)}>Finished</button>
+                  }
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
